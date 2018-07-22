ad9fccb2ba046d41085b63188cfdb469f1cad698
Vadász Zoltán <vadaszzoltan@minic.ro>
2018-07-22 18:55:14 +0300

    Vadiables

---
+++ code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+print 'Szeretlek kincsem'
+"""
+x=raw_input("x=")
+print x
+
+if int(x) % 2 == 0:
+    print "Paros"
+else:
+    print "Paratlan"
+"""
+""" z=raw_input("z=")
+t=raw_input("t=")
+print (t + z)
+"""
+a=5
+print a+a
+	
